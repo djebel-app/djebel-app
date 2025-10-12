4dd07decfc4603ada08fc472616ff4795afa0bac
slavi <admin@orbisius.com>
2025-10-12 12:38:36 +0300

    added tests for String_Util_Test.php

---
+++ tests/unit_tests/lib/String_Util_Test.php
@@ -0,0 +1,90 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class String_Util_Test extends TestCase {
+
+    public function testGetFirstCharWithNormalString()
+    {
+        $result = Dj_App_String_Util::getFirstChar('hello');
+        $this->assertEquals('h', $result);
+    }
+
+    public function testGetFirstCharWithSingleChar()
+    {
+        $result = Dj_App_String_Util::getFirstChar('x');
+        $this->assertEquals('x', $result);
+    }
+
+    public function testGetFirstCharWithEmptyString()
+    {
+        $result = Dj_App_String_Util::getFirstChar('');
+        $this->assertEquals('', $result);
+    }
+
+    public function testGetFirstCharWithNull()
+    {
+        $result = Dj_App_String_Util::getFirstChar(null);
+        $this->assertEquals('', $result);
+    }
+
+    public function testGetFirstCharWithNumber()
+    {
+        $result = Dj_App_String_Util::getFirstChar('123abc');
+        $this->assertEquals('1', $result);
+    }
+
+    public function testGetFirstCharWithSpecialChar()
+    {
+        $result = Dj_App_String_Util::getFirstChar('!@#test');
+        $this->assertEquals('!', $result);
+    }
+
+    public function testGetFirstCharWithWhitespace()
+    {
+        $result = Dj_App_String_Util::getFirstChar(' hello');
+        $this->assertEquals(' ', $result);
+    }
+
+    public function testGetLastCharWithNormalString()
+    {
+        $result = Dj_App_String_Util::getLastChar('hello');
+        $this->assertEquals('o', $result);
+    }
+
+    public function testGetLastCharWithSingleChar()
+    {
+        $result = Dj_App_String_Util::getLastChar('x');
+        $this->assertEquals('x', $result);
+    }
+
+    public function testGetLastCharWithEmptyString()
+    {
+        $result = Dj_App_String_Util::getLastChar('');
+        $this->assertEquals('', $result);
+    }
+
+    public function testGetLastCharWithNull()
+    {
+        $result = Dj_App_String_Util::getLastChar(null);
+        $this->assertEquals('', $result);
+    }
+
+    public function testGetLastCharWithNumber()
+    {
+        $result = Dj_App_String_Util::getLastChar('abc123');
+        $this->assertEquals('3', $result);
+    }
+
+    public function testGetLastCharWithSpecialChar()
+    {
+        $result = Dj_App_String_Util::getLastChar('test!@#');
+        $this->assertEquals('#', $result);
+    }
+
+    public function testGetLastCharWithWhitespace()
+    {
+        $result = Dj_App_String_Util::getLastChar('hello ');
+        $this->assertEquals(' ', $result);
+    }
+}
